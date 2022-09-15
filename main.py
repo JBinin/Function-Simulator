38a8c6188bc266aedca0c7ed3bfb052d7b669ab3
JBinin <namechenjiabin@icloud.com>
2022-09-15 14:39:32 +0800

    change the file path of the dataset

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
+import os
+
 from workload import Workload
 from simulator import Simulator
 
-
-data_path = "~/Downloads/azurefunctions-dataset2019/"
+cur_dir = os.path.dirname(__file__)
+data_path = "Dataset/azure-functions-dataset2019/"
+data_path = os.path.join(cur_dir, data_path)
 csv_file_pre = "invocations_per_function_md.anon.d"
 
 trace = Workload()
 
 for i in range(1, 2):
-    csv_file = csv_file_pre + "{:02d}.csv".format(i)
+    csv_file = os.path.join(data_path, "{:02d}.csv".format(i))
     csv_file = data_path + csv_file
     trace.add_workload(csv_file)
 
