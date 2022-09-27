48c0814fb9bac939766a8ad081f13c1c6e77e23c
JBinin <namechenjiabin@icloud.com>
2022-09-27 14:53:48 +0800

    update utility test

--- Test/test_utility.py
+++ Test/test_utility.py
@@ -1,23 +1,39 @@
 import os
 import unittest
-import utility
+
+from utility import split_dict, save_dict_list_to_csv
 import numpy as np
 
 
 class TestUtility(unittest.TestCase):
-    def setUp(self) -> None:
-        self.txt_file = "test.txt"
-        with open(self.txt_file, "w") as f:
-            for i in range(10):
-                f.write(str(i) + "\n")
+    def test_split_dict(self):
+        test_dicts = {
+            "a": np.array([0, 0]),
+            "b": np.array([1, 1]),
+            "c": np.array([2, 2]),
+            "d": np.array([3, 3])
+        }
+        test_dict_list = []
+        split_dict(test_dicts, test_dict_list, 3)
+        self.assertEqual(len(test_dict_list), 3)
+        self.assertEqual(len(test_dict_list[0]), 1)
+        self.assertEqual(len(test_dict_list[2]), 2)
+        self.assertEqual(test_dict_list[2]["d"][0], 3)
 
-    def tearDown(self) -> None:
-        os.remove(self.txt_file)
-
-    def test_read_from_txt(self):
-        result = utility.read_from_txt(self.txt_file)
-        expect = np.array(list(range(10)))
-        self.assertTrue(all(result == expect))
+    def test_save_dict_list_to_csv(self):
+        csv_file = "test_utility.csv"
+        test_dict_list = [
+            {"a": 1, "b": 2},
+            {"c": 3}
+        ]
+        save_dict_list_to_csv(test_dict_list, csv_file)
+        with open(csv_file) as f:
+            lines = f.readlines()
+            self.assertEqual(lines[0], ",mse\n")
+            self.assertEqual(lines[1], "a,1\n")
+            self.assertEqual(lines[2], "b,2\n")
+            self.assertEqual(lines[3], "c,3\n")
+        os.remove(csv_file)
 
 
 if __name__ == "__main__":
